6021bb54a1793f76dc033859e41d074233ea5554
Dovydas Bartkevicius <dovydas.bartkevicius@internations.org>
2016-08-22 22:25:23 +0200

    Flags

--- main.go
+++ main.go
@@ -8,25 +8,29 @@ import (
 	"os"
 )
 
-var port = *flag.Int("port", 3003, "Port to listen on.")
-var host = *flag.String("host", "", "Host.")
-var dir = *flag.String("path", "", "Path to serve files from. Default: current directory.")
+var port int
+var host string
+var path string
 
 func main() {
+	flag.IntVar(&port, "port", 3003, "Port to listen on")
+	flag.StringVar(&host, "host", "", "Host.")
+	flag.StringVar(&path, "path", "", "Path to serve files from. Default: current directory.")
 	flag.Parse()
-	http.Handle("/", http.FileServer(http.Dir(dir)))
 
-	if dir == "" {
+	http.Handle("/", http.FileServer(http.Dir(path)))
+
+	if path == "" {
 		cwd, err := os.Getwd()
 
 		if err != nil {
 			panic(err)
 		}
 
-		dir = cwd
+		path = cwd
 	}
 
-	log.Printf("Listening on %s:%d and serving %s\n", host, port, dir)
+	log.Printf("Listening on %s:%d and serving %s\n", host, port, path)
 
 	http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), nil)
 }
